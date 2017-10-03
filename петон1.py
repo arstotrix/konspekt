bfc114b014e2af44d1e8ca774182bfde61901863
Maria Maslova <mashamaslova85@gmail.com>
2017-10-03 11:50:19 +0300

    Add files via upload

---
+++ петон1.py
@@ -0,0 +1,59 @@
+#word = "holiday"
+#print (word)
+#a=0
+
+#for i in word:
+#    if a%2==0:
+#        print(i)
+#    else:
+#        print("_")
+#    print (i, a)
+#    a+=1;
+#print(a)
+#print("="*10)
+
+#print(word[3])
+#print(word[len(word)-1])
+#print(word[-5])
+#print(word[2:5])
+#print(word[::2])
+#print(word[-5:-1])
+#part=(word[4:])
+#print(word[:4]+"!"+part)
+#s=0
+#num=int(input("Your number here"))
+#for i in range (num+1):
+#    print ()
+#    s+=i
+#    print (i)
+#    if (i>10) and (i%2!=0):
+#        print(i**2)
+#print("="*10)
+#print (s)
+#print ("end")
+#i=2
+#while (i**2<=100):
+#    print(i)
+#    print (i**2)
+#    i+=1
+#print ("i =", i)
+
+# i in range (10):
+#    print()
+#    print(i, end=", ")
+#    if i==8:
+#        continue
+#    print (i**2)
+#print ("end!")
+n=int(input("insert a number "))
+for i in range (n):
+    x=int(input("insert a number "))
+    if (x>0):
+        print("Greater than zero")
+    elif (x<0):
+        print("Smaller than zero")
+    else:
+        print("Suddenly, a zero")
+#     word = input('insert a word ')
+#     print(len(word))
+
